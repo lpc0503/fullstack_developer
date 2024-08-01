1edeb71d5752203d69241038e6b4b8a33cfd6dca
ICEJJ <lpc0503@gmail.com>
2024-08-01 14:59:11 -0400

    fix models indent

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -35,16 +35,16 @@ class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     CAR_TYPES = [
-        ('SEDAN', 'Sedan'),
-        ('SUV', 'SUV'),
-        ('WAGON', 'Wagon'),
-        # Add more choices as required
+    ('SEDAN', 'Sedan'),
+    ('SUV', 'SUV'),
+    ('WAGON', 'Wagon'),
+    # Add more choices as required
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2023,
     validators=[
-        MaxValueValidator(2023),
-        MinValueValidator(2015)
+    MaxValueValidator(2023),
+    MinValueValidator(2015)
     ])
     # Other fields as needed
 
